ae4a75ac5570a2ee0527c6ee5997d5a9479a0482
aymurat111 <aymurat111@gmail.com>
2022-12-10 08:27:26 +0100

    cucumber

--- src/test/java/pages/QualityDemyPage.java
+++ src/test/java/pages/QualityDemyPage.java
@@ -73,4 +73,25 @@ public class QualityDemyPage {
 
     @FindBy(xpath = "//button[text()='Sign Up']")
     public WebElement buttonNewSignUp;
+
+    @FindBy(xpath = "//a[@class='btn btn-sign-up']")
+    public WebElement signupLink;
+
+    @FindBy(xpath = "(//input[@type='text'])[2]")
+    public WebElement firstnameBox;
+
+    @FindBy(xpath = "(//input[@type='text'])[3]")
+    public WebElement lastnameBox;
+
+    @FindBy(xpath = "(//input[@type='text'])[4]")
+    public WebElement signupEmailBox;
+
+    @FindBy(xpath = "//input[@type='password']")
+    public WebElement signupPasswordBox;
+
+    @FindBy(xpath = "(//button[@type='submit'])[2]")
+    public WebElement signupButton;
+
+    @FindBy(xpath = "//div[@class='toast-message']")
+    public WebElement hataMesaji;
 }
