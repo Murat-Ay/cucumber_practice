9838c7bbdf08d121059b1265ddec77f89ed58846
aymurat111 <aymurat111@gmail.com>
2023-01-17 10:31:20 +0100

    xx

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -14,10 +14,8 @@ public class AmazonPage {
         @FindBy(xpath = "//input[@id='twotabsearchtextbox']")
         public WebElement amazonAramaKutusu;
 
-
         @FindBy(xpath = "//span[@class='a-color-state a-text-bold']")
         public WebElement amazonAramaSonucuTest;
-
 
         @FindBy(xpath = "//select[@aria-describedby='searchDropdownDescription']")
         public WebElement dropDownMenu;
